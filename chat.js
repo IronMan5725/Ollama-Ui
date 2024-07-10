068a0d9c7e48b487fd66c12890404d46c2e2ecdc
Iron Man <153529035+IronMan5725@users.noreply.github.com>
2024-07-10 10:54:21 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -202,11 +202,7 @@ async function submitRequest() {
           let copyButton = document.createElement('button');
           copyButton.className = 'btn btn-secondary copy-button';
           copyButton.innerHTML = clipboardIcon;
-          navigator.clipboard.writeText(responseDiv.hidden_text).then(() => {
-              console.log('Text copied to clipboard');
-            }).catch(err => {
-              console.error('Failed to copy text:', err);
-            });
+          navigator.clipboard.writeText(responseDiv.hidden_text);
           copyButton.onclick = () => {
             navigator.clipboard.writeText(responseDiv.hidden_text).then(() => {
               console.log('Text copied to clipboard');
